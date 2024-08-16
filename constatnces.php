6a36be44e49e435a77ebc3def899d4ad1af80a59
mourad barkouch <mouradbarkouch@gmail.com>
2024-08-16 20:53:12 +0100

    les contatantes

---
+++ constatnces.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/*
+  Constants
+  - Cette valeur ne peut pas changer pendant l'exécution
+  - Constants toujours Uppercase
+*/
+
+const DB_NAME = "Mo";
+const MAIN_NUMBER = 5;
+
+echo DB_NAME;
+echo '<br/>';
+echo MAIN_NUMBER * 50;
+
+
